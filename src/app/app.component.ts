ffcca7adb6fc7fb7fb014966ab54a31dbe3338d3
unknown <marcoalmeidafamelii@gmail.com>
2024-09-28 11:58:16 -0300

    first angular commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {CommonModule} from "@angular/common";
+import {NewComponent} from "./components/new-component/new-component.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule,RouterOutlet,NewComponent],
+  template:
+    `<!--'<router-outlet></router-outlet>' -->\
+    <h1>Curso de Angular</h1>
+    <app-new-component/>
+
+  `
+})
+export class AppComponent {
+  title = 'angular1';
+}
